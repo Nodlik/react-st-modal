37be3d5200a3b4a0d2a30d872f58e3e2301a339f
Oleg L <oleg@rest.systems>
2020-10-09 16:58:53 +0300

    Added demo site

--- src/Modal/Modal.tsx
+++
@@ -1,8 +0,0 @@
-import '../vars.scss';
-
-import React from 'react';
-import styles from './modal.module.scss';
-
-export default function Modal() {
-    return <div className={styles.overlay}></div>;
-}
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import Modal from './Modal/Modal';
-
-export { Modal };
